0915a52b197326c106fc4b17b945eeecf42cbe23
roulendz <rolands.zeltins@inbox.lv>
2023-05-05 18:46:48 +0300

    v2

--- game.js
+++ game.js
@@ -3,176 +3,238 @@ var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 
 // Define some game constants
-var FPS = 60; // Frames per second
-var GRAVITY = 0.5; // Gravity force
-var FRICTION = 0.9; // Friction coefficient
+var FPS = 30; // Frames per second
+// Set the canvas width and height
+var width = 800;
+var height = 600;
 
-// Define some game variables
-var score = 0; // The player's score
-var gameOver = false; // The game over flag
+// Define some constants for the game
+var GRAVITY = 0.5; // The gravity force that pulls the player down
+var FRICTION = 0.8; // The friction force that slows down the player horizontally
+var JUMP_SPEED = -10; // The initial vertical speed when the player jumps
+var MOVE_SPEED = 5; // The horizontal speed when the player moves left or right
+var SCROLL_SPEED = 2; // The speed at which the background scrolls
+var PLAYER_SIZE = 20; // The size of the player box
+var ENEMY_SIZE = 20; // The size of the enemy box
+var OBSTACLE_SIZE = 40; // The size of the obstacle box
 
-// Define some game objects
-var player = {
-    x: 100, // The player's x position
-    y: 100, // The player's y position
-    vx: 0, // The player's x velocity
-    vy: 0, // The player's y velocity
-    width: 50, // The player's width
-    height: 50, // The player's height
-    color: "blue", // The player's color
-};
+// Define some variables for the game state
+var lastRender = 0; // The timestamp of the last render
+var gameOver = false; // Whether the game is over or not
+var score = 0; // The current score of the game
+var scrollX = 0; // The current scroll position of the background
+var playerX = width / 2; // The current x position of the player
+var playerY = height - PLAYER_SIZE; // The current y position of the player
+var playerVX = 0; // The current horizontal velocity of the player
+var playerVY = 0; // The current vertical velocity of the player
+var jumping = false; // Whether the player is jumping or not
+var enemies = []; // An array of enemies objects
+var obstacles = []; // An array of obstacles objects
 
-var enemies = []; // An array of enemies
+// Define some colors for the game elements
+var backgroundColor = "lightblue";
+var groundColor = "green";
+var playerColor = "blue";
+var enemyColor = "red";
+var obstacleColor = "brown";
 
-// Define some game functions
-function render() {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+// Add an event listener for keydown events
+window.addEventListener("keydown", function(event) {
+  // Check which key was pressed
+  switch (event.keyCode) {
+    case 37: // Left arrow key
+      // Move the player to the left
+      playerVX = -MOVE_SPEED;
+      break;
+    case 39: // Right arrow key
+      // Move the player to the right
+      playerVX = MOVE_SPEED;
+      break;
+    case 32: // Spacebar key
+      // Make the player jump if not already jumping
+      if (!jumping) {
+        jumping = true;
+        playerVY = JUMP_SPEED;
+      }
+      break;
+  }
+});
 
-  // Draw the player
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.width, player.height);
+// Add an event listener for keyup events
+window.addEventListener("keyup", function(event) {
+  // Check which key was released
+  switch (event.keyCode) {
+    case 37: // Left arrow key
+    case 39: // Right arrow key
+      // Stop moving the player horizontally
+      playerVX = 0;
+      break;
+    case 32: // Spacebar key
+      // Stop jumping if the player is going up
+      if (playerVY < 0) {
+        playerVY = 0;
+      }
+      break;
+  }
+});
 
-  // Draw the enemies
-  ctx.fillStyle = "red";
-  for (var i = 0; i < enemies.length; i++) {
-    var enemy = enemies[i];
-    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+// Define a function to update the game state based on elapsed time since last render
+function update(progress) {
+  // Update the score based on progress
+  score += progress;
+
+  // Update the scroll position based on scroll speed and progress
+  scrollX += SCROLL_SPEED * progress;
+
+  // Update the player position based on velocity and progress
+  playerX += playerVX * progress;
+  playerY += playerVY * progress;
+
+  // Apply gravity to the player vertical velocity
+  playerVY += GRAVITY * progress;
+
+  // Apply friction to the player horizontal velocity
+  if (playerVX > 0) {
+    playerVX -= FRICTION * progress;
+    if (playerVX < 0) {
+      playerVX = 0;
+    }
+  } else if (playerVX < 0) {
+    playerVX += FRICTION * progress;
+    if (playerVX > 0) {
+      playerVX = 0;
+    }
   }
 
-  // Draw the score
-  ctx.fillStyle = "black";
-  ctx.font = "20px Arial";
-  ctx.fillText("Score: " + score, 10, 30);
-}
-
-function update() {
-  // Update the player's position and velocity
-    player.x += player.vx; // Add the x velocity to the x position
-    player.y += player.vy; // Add the y velocity to the y position
-    player.vy += GRAVITY; // Add the gravity force to the y velocity
-    player.vx *= FRICTION; // Multiply the x velocity by the friction coefficient
-
-    // Limit the maximum y velocity
-    if (player.vy > 20) {
-    // The player's y velocity is greater than 20
-    player.vy = 20; // Set the y velocity to 20
-    }
-    if (player.vy < -20) {
-    // The player's y velocity is less than -20
-    player.vy = -20; // Set the y velocity to -20
-    }
-
-    // Check if the player hits the canvas boundaries and bounce back
-    if (player.x < 0) {
-    // The player hits the left edge
-    player.x = 0; // Set the x position to 0
-    player.vx = -player.vx; // Reverse the x velocity
-    }
-    if (player.x + player.width > canvas.width) {
-    // The player hits the right edge
-    player.x = canvas.width - player.width; // Set the x position to the canvas width minus the player width
-    player.vx = -player.vx; // Reverse the x velocity
-    }
-    if (player.y < 0) {
-    // The player hits the top edge
-    player.y = canvas.height - player.height; // Set the y position to **the canvas height minus** **the** **player height**
-    // Do not reverse** **the** **y** **velocity**
-    }
-    if (player.y + player.height > canvas.height) {
-    // The player hits the bottom edge
-    // Do not change** **the** **y** **position**
-    // Do not reverse** **the** **y** **velocity**
-    }
-
-  // Update the enemies' position and velocity
-  for (var i = 0; i < enemies.length; i++) {
-    var enemy = enemies[i];
-    enemy.x += enemy.vx; // Add the x velocity to the x position
-    enemy.y += enemy.vy; // Add the y velocity to the y position
-
-    // Check if the enemy hits the canvas boundaries and remove it
-    if (enemy.x < -enemy.width || enemy.x > canvas.width || enemy.y < -enemy.height || enemy.y > canvas.height) {
-      // The enemy is out of bounds
-      enemies.splice(i, 1); // Remove the enemy from the array
-      i--; // Decrement i to avoid skipping an element
-      score++; // Increment score by one
-    }
-
-    // Check if the enemy collides with the player and end the game
-    if (enemy.x + enemy.width > player.x && enemy.x < player.x + player.width && enemy.y + enemy.height > player.y && enemy.y < player.y + player.height) {
-      // The enemy overlaps with the player
-      gameOver = true; // Set game over flag to true
-      break; // Break out of loop
-    }
+  // Check if the player is on the ground
+  if (playerY + PLAYER_SIZE >= height) {
+    // Set the player position and velocity to the ground level
+    playerY = height - PLAYER_SIZE;
+    playerVY = 0;
+    jumping = false;
   }
 
-  // Spawn new enemies randomly from above with different speeds and sizes
-  if (Math.random() < 0.01) {
-    // There is a 1% chance of spawning an enemy every frame
+  // Check if the player is out of the canvas horizontally
+  if (playerX + PLAYER_SIZE < 0 || playerX > width) {
+    // Game over
+    gameOver = true;
+  }
+
+  // Generate a new enemy every second on average
+  if (Math.random() < progress / 1000) {
+    // Create a new enemy object with random x position and fixed y position
     var enemy = {
-      x: Math.random() * canvas.width, // Random x position between 0 and canvas width
-      y: -50, // y position above canvas height
-      vx: Math.random() * 10 - 5, // Random x velocity between -5 and 5
-      vy: Math.random() * 10 + 5, // Random y velocity between 5 and 15
-      width: Math.random() * 50 + 10, // Random width between 10 and 60
-      height: Math.random() * 50 + 10, // Random height between 10 and 60
+      x: Math.random() * width,
+      y: height - ENEMY_SIZE,
     };
-    enemies.push(enemy); // Add enemy to array
+    // Add the enemy to the enemies array
+    enemies.push(enemy);
   }
 
-}
+  // Generate a new obstacle every two seconds on average
+  if (Math.random() < progress / 2000) {
+    // Create a new obstacle object with random x position and fixed y position
+    var obstacle = {
+      x: Math.random() * width,
+      y: height - OBSTACLE_SIZE,
+    };
+    // Add the obstacle to the obstacles array
+    obstacles.push(obstacle);
+  }
 
-function render() {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the player
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.width, player.height);
-
-  // Draw the enemies
-  ctx.fillStyle = "red";
+  // Update the enemies positions based on scroll speed and progress
   for (var i = 0; i < enemies.length; i++) {
-    var enemy = enemies[i];
-    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    enemies[i].x -= SCROLL_SPEED * progress;
   }
 
-  // Draw the score
+  // Update the obstacles positions based on scroll speed and progress
+  for (var i = 0; i < obstacles.length; i++) {
+    obstacles[i].x -= SCROLL_SPEED * progress;
+  }
+
+  // Check for collisions between the player and the enemies or obstacles
+  for (var i = 0; i < enemies.length; i++) {
+    if (
+      playerX + PLAYER_SIZE > enemies[i].x &&
+      playerX < enemies[i].x + ENEMY_SIZE &&
+      playerY + PLAYER_SIZE > enemies[i].y &&
+      playerY < enemies[i].y + ENEMY_SIZE
+    ) {
+      // Game over
+      gameOver = true;
+    }
+  }
+  
+   for (var i = 0; i < obstacles.length; i++) {
+    if (
+      playerX + PLAYER_SIZE > obstacles[i].x &&
+      playerX < obstacles[i].x + OBSTACLE_SIZE &&
+      playerY + PLAYER_SIZE > obstacles[i].y &&
+      playerY < obstacles[i].y + OBSTACLE_SIZE
+    ) {
+      // Game over
+      gameOver = true;
+    }
+  }
+}
+// Define a function to draw the game state on the canvas
+function draw() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, width, height);
+
+  // Draw the background color
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, width, height);
+
+  // Draw the ground color
+  ctx.fillStyle = groundColor;
+  ctx.fillRect(0, height - PLAYER_SIZE, width, PLAYER_SIZE);
+
+  // Draw the player box
+  ctx.fillStyle = playerColor;
+  ctx.fillRect(playerX, playerY, PLAYER_SIZE, PLAYER_SIZE);
+
+  // Draw the enemies boxes
+  ctx.fillStyle = enemyColor;
+  for (var i = 0; i < enemies.length; i++) {
+    ctx.fillRect(enemies[i].x, enemies[i].y, ENEMY_SIZE, ENEMY_SIZE);
+  }
+
+  // Draw the obstacles boxes
+  ctx.fillStyle = obstacleColor;
+  for (var i = 0; i < obstacles.length; i++) {
+    ctx.fillRect(obstacles[i].x, obstacles[i].y, OBSTACLE_SIZE, OBSTACLE_SIZE);
+  }
+
+  // Draw the score text
   ctx.fillStyle = "black";
   ctx.font = "20px Arial";
-  ctx.fillText("Score: " + score, 10, 30);
+  ctx.fillText("Score: " + Math.floor(score / 1000), 10, 30);
 }
 
-function loop() {
-    // Call the update and render functions every frame
-    update();
-    render();
-    requestAnimationFrame(loop);
+// Define a function to loop the game
+function loop(timestamp) {
+  // Calculate the progress since the last render
+  var progress = timestamp - lastRender;
+
+  // Update the game state
+  update(progress);
+
+  // Draw the game state
+  draw();
+
+  // Check if the game is over
+  if (gameOver) {
+    // Display a game over message
+    ctx.fillStyle = "black";
+    ctx.font = "40px Arial";
+    ctx.fillText("Game Over!", width / 2 - 100, height / 2);
+  } else {
+    // Request the next animation frame
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
+  }
 }
 
 // Start the game loop
-loop();
-
-// Add an event listener for keydown events
-window.addEventListener("keydown", function(e) {
-    // Check which key was pressed
-    switch (e.key) {
-        case 'ArrowLeft': // Left arrow key
-            // Move the player to the left by decreasing its x velocity
-            player.vx -= 5;
-            break;
-        case 'ArrowRight': // Right arrow key
-            // Move the player to the right by increasing its x velocity
-            player.vx += 5;
-            break;
-        case 'ArrowUp': // Up arrow key
-            // Make the player jump by setting its y velocity to a negative value only if it is on the ground
-            if (player.y + player.height === canvas.height) {
-            // The player's bottom edge is equal to the canvas height
-            player.vy = -10; // Set the y velocity to -10
-            }
-            break;
-        }
-
-});
+window.requestAnimationFrame(loop);
